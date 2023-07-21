565941355366041dec014b23413fa58a8561708e
emreb <emre@cerbos.dev>
2023-07-21 16:26:15 +0100

    Udpatgin the demo

--- cerbos/start-cloud.sh
+++ cerbos/start-cloud.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
+if [ -z $1 ]; then CERB_ENV="latest"; else CERB_ENV=$1; fi
 
 docker run --rm -i -t \
   -p 3592:3592 -p 3593:3593 \
   -v /tmp -v /.cache \
-  -e CERBOS_CLOUD_BUNDLE="latest" \
+  -e CERBOS_CLOUD_BUNDLE=$CERB_ENV \
   -e CERBOS_CLOUD_SECRET_KEY="CERBOS-1OJ4XX9NR1KBL-0JL30W6N9FL79UWHCL549UELN6GGLMUKD299ZRDH72PGEDJTVEFST2Q46R" \
   -e CERBOS_CLOUD_CLIENT_ID="OZB7EQKLC3LJ" \
   -e CERBOS_CLOUD_CLIENT_SECRET="cerbos_le46G7gWXtc0dYHjuW64mhozDo7yp+BP9WwqK9TV12g" \
   -e CERBOS_NO_TELEMETRY=1 \
   ghcr.io/cerbos/cerbos:0.28.0 server --set=server.logRequestPayloads=true
+
